3fe53c71515e59c06b455e28f131bb5405ce1c8a
vlad <vnelipovich63@gmail.com>
2025-03-17 12:46:13 +0300

    feat: add loader

---
+++ src/components/shared/Loader.tsx
@@ -0,0 +1,9 @@
+export const Loader = () => {
+  return (
+    <div className="w-full items-center flex justify-center min-h-dvh">
+      <div className="loader"></div>
+    </div>
+  )
+}
+
+export default Loader
